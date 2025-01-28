55eae09eb8e9d6888759ccdbd210f1c8f0af9c91
viba98 <32447352+viba98@users.noreply.github.com>
2025-01-28 11:03:45 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ email })
+        body: JSON.stringify({ email, source: "www.fiddle.is" })
       });
 
       const data = await response.json();
