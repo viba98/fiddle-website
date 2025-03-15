f59869f3930368bc58122371ef51e0ce493de8ad
viba98 <32447352+viba98@users.noreply.github.com>
2025-03-14 19:34:46 -0700

    🔖 base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,27 +3,65 @@
 import Image from "next/image";
 import { useState, useEffect } from "react";
 
+const imageUrls = [
+  '/img1.png',
+  '/img1.png',
+  '/img1.png',
+  '/img1.png',
+  '/img1.png',
+  '/img1.png',
+  '/img1.png',
+  '/img1.png',
+  '/img1.png',
+  '/img1.png',
+  '/img1.png',
+  '/img1.png',
+  '/img1.png',
+  '/img1.png',
+  '/img1.png',
+  '/img1.png',
+  '/img1.png',
+  '/img1.png'
+];
+
+// const timestamps = Array.from({ length: 11 }, (_, index) => {
+//   const time = (index + 1) * 5; 
+//   return `00:${time}`;
+// });
+
+function logBanner(){
+  console.log(`
+_____ _     _     _ _      
+|  ___(_) __| | __| | | ___ 
+| |_  | |/ _  |/ _  | |/ _ \
+
+|  _| | | (_| | (_| | |  __/
+|_|   |_|\__,_|\__,_|_|\___|
+
+code is the best prototyping tool.
+
+twitter: https://twitter.com/fiddle_factory`);
+}
+
+logBanner();
+
+const timestamps = [
+  '00:00 | INFINITE CANVAS',
+  '00:05 | VIBE CODE',
+  '00:10 | VISUAL EDITS',
+  '00:15 | DESIGN SYSTEM',
+  '00:20 | COLLABORATE'
+]
+
 export default function Home() {
   const [email, setEmail] = useState('');
   const [loading, setLoading] = useState(false);
   const [emailSent, setEmailSent] = useState(false);
   const [waitlisted, setWaitlisted] = useState(false);
-
-  // Global command+enter handler
-  useEffect(() => {
-    const handleGlobalKeyDown = (e: KeyboardEvent) => {
-      if (e.key === 'Enter') {
-        if (email && !loading && !emailSent) {
-          handleSignIn();
-        } else if (emailSent) {
-          window.open(waitlisted ? 'https://forms.gle/9wjkDzamRSeHVPRw5' : 'https://mail.google.com', '_blank');
-        }
-      }
-    };
-
-    window.addEventListener('keydown', handleGlobalKeyDown);
-    return () => window.removeEventListener('keydown', handleGlobalKeyDown);
-  }, [email, loading, emailSent, waitlisted]);
+  const [scrubberPosition, setScrubberPosition] = useState(0);
+  const [showInput, setShowInput] = useState(false);
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  const [cursorText, setCursorText] = useState('[play]');
 
   const handleSignIn = async () => {
     try {
@@ -44,7 +82,6 @@ export default function Home() {
       
       if (!response.ok) {
         console.log('Sign in failed:', data.error || 'Sign in failed');
-        // Instead of showing error, show waitlist message
         setEmailSent(true);
         setWaitlisted(true);
         return;
@@ -64,95 +101,215 @@ export default function Home() {
     }
   };
 
+  // Global command+enter handler
+  useEffect(() => {
+    const handleGlobalKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'Enter') {
+        if (email && !loading && !emailSent) {
+          handleSignIn();
+        } else if (emailSent) {
+          window.open(waitlisted ? 'https://forms.gle/9wjkDzamRSeHVPRw5' : 'https://mail.google.com', '_blank');
+        }
+      }
+    };
+
+    window.addEventListener('keydown', handleGlobalKeyDown);
+    return () => window.removeEventListener('keydown', handleGlobalKeyDown);
+  }, [email, loading, emailSent, waitlisted]);
+
+  const handleMouseMove = (e: MouseEvent) => {
+    setCursorPosition({ x: e.clientX, y: e.clientY });
+  };
+
+  // Global command+enter handler
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  const handleMouseMoveScrubber = (e: MouseEvent) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const offsetX = e.clientX - rect.left; 
+
+    requestAnimationFrame(() => {
+      setScrubberPosition(offsetX);
+    });
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center p-8 pt-16 bg-[#151517] text-white">
+    <main className="flex min-h-screen flex-col items-center inset-0 bg-[#151517] text-white cursor-crosshair" style={{ fontFamily: 'monospace' }}>
       
       {/* Logo + Name Section */}
-      <div className="flex items-center gap-2 mb-4 absolute top-4 left-4 m-4">
-        <Image
+      <div className="flex items-center gap-2 mb-4 absolute top-4 left-4 m-4 z-10">
+        {/* <Image
           src="/logo.png"
           alt="Fiddle Logo"
           width={16}
           height={16}
-          className="w-8 h-8"
-        />
-        <h1 className="text-base font-medium">Fiddle</h1>
+        /> */}
+        <span className="text-xs font-semibold mix-blend-difference">FIDDLE | SOFTWARE SHOULD FEEL LIKE MAGIC</span>
       </div>
 
-      {/* Discord Link Section */}
-      <div className="absolute top-4 right-4">
+      {/* Waitlist Section */}
+      <div className="absolute top-4 right-4 z-10 flex gap-2">
         <a 
           href="https://discord.gg/fYUTpD86vu"
           target="_blank"
           rel="noopener noreferrer"
-          className="flex items-center gap-2 px-4 py-2 bg-white/10 text-white/80 rounded-lg hover:bg-[#4752C4] transition-colors"
+          className="flex items-center gap-2 px-2 py-2 text-white/80 hover:bg-white/10 transition-colors"
+          onMouseEnter={() => setCursorText('[ Join Discord ]')}
+          onMouseLeave={() => setCursorText('[ play ]')}
         >
           <Image
             src="/discord.svg"
             alt="Discord"
-            width={16}
-            height={16}
-            className="w-6 h-6"
+            width={20}
+            height={20}
+            className="mix-blend-difference"
           />
-          <span className="font-medium text-base">Join Discord</span>
         </a>
+        
       </div>
 
-      {/* Title Section */}
-      <div className="max-w-3xl text-center my-16">
-        <h2 className="text-5xl font-medium leading-tight mb-4">
-          Make your designs come alive
-        </h2>
-        <h3 className="text-xl text-white/80">
-          Convert your Figma designs to code and experiment on an infinite canvas
-        </h3>
-      </div>
+      {/* Timestamps Section */}
+      <div 
+        className="absolute bottom-4 left-4 right-4 z-10 flex flex-col w-[calc(100%-32px)]"
+        onMouseMove={handleMouseMoveScrubber} 
+      >
+        <div className="z-20 gap-0">
+          {/* orange Scrubber Line */}
+          <div 
+            className="absolute bg-[#FF3001] z-20"
+            style={{
+              left: `${scrubberPosition - 2}px`, 
+              height: '120%', 
+              width: '1px',
+              top: '-10%',
+              transition: 'left 0.05s ease-out'
+            }}
+          ></div>
 
-      {/* Get Access Section */}
-      <div className="w-full max-w-md mb-16">
-        <div className="relative">
-          {!emailSent ? (
+          {/* Triangle at the top of the Scrubber */}
+          <div 
+            className="absolute z-20"
+            style={{
+              left: `${scrubberPosition - 7}px`, 
+              top: '-12%',
+              width: 0,
+              height: 0,
+              borderLeft: '6px solid transparent',
+              borderRight: '6px solid transparent',
+              borderTop: '8px solid #FF3001' ,
+              transition: 'left 0.05s ease-out'
+            }}
+          ></div>
+
+          {/* Rectangle Above the Triangle */}
+          <div 
+            className="absolute z-20"
+            style={{
+              left: `${scrubberPosition - 7}px`, 
+              top: 'calc(-12% - 5px)', 
+              width: '12px', 
+              height: '5px', 
+              backgroundColor: '#FF3001',
+              transition: 'left 0.05s ease-out'
+            }}
+          ></div>
+        </div>
+
+        <div className="flex justify-between w-full mb-2 z-10 items-center">
+          {timestamps.map((timestamp, index) => (
+            <span key={index} className="flex-1 text-xs text-white">{timestamp}</span>
+          ))}
+          <div className="relative">
+          {!showInput ? (
+            <button
+              onClick={() => setShowInput(true)}
+              className="button-l-shape text-[#FF3001] transition-colors text-xs font-semibold uppercase mix-blend-difference"
+              onMouseEnter={() => setCursorText('')}
+          onMouseLeave={() => setCursorText('[ play ]')}
+            >
+               <span>JOIN WAITLIST</span>
+            </button>
+          ) : !emailSent ? (
             <div className="flex gap-2">
-              <input
-                type="email"
-                autoFocus
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Enter your email"
-                className="w-full px-4 py-3 rounded-lg border border-gray-700 bg-[#1E1E20] text-white placeholder-gray-400 focus:border-[#FF6101] focus:ring-1 focus:ring-[#FF6101] outline-none transition-colors"
-                disabled={loading}
-              />
-              <button
-                onClick={handleSignIn}
-                disabled={loading || !email}
-                className="px-4 py-2 bg-[#FF6101] text-white rounded-lg hover:bg-[#FF3001] transition-colors disabled:opacity-50 disabled:cursor-not-allowed whitespace-nowrap"
-              >
-                {loading ? 'Sending...' : 'Get Access ⌘↵'}
-              </button>
+              <div className="relative w-full">
+                <input
+                  type="email"
+                  autoFocus
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Enter your email"
+                  className="w-full px-2 py-2 text-xs border border-gray-700 bg-[#1E1E20] text-white placeholder-gray-400 focus:border-[#FF3101] focus:ring-1 focus:ring-[#FF3101] outline-none transition-colors"
+                  disabled={loading}
+                />
+                <span className="absolute right-3 top-1/2 transform -translate-y-1/2 text-white text-sm">
+                  ↵
+                </span>
+              </div>
             </div>
           ) : (
-            <div className="flex items-center justify-between px-4 py-3 bg-[#1E1E20] border border-gray-700 rounded-lg shadow-sm">
+            <div className="flex items-center justify-between px-2 py-2 bg-[#1E1E20] border border-gray-700 shadow-sm gap-6 ">
               <div className="flex items-center gap-2">
-                <svg className="w-5 h-5 text-white/60" viewBox="0 0 16 16" fill="currentColor">
+                {/* <svg className="w-3 h-3 text-white/60" viewBox="0 0 16 16" fill="currentColor">
                   <path d="M0,9.014L1.414,7.6L5.004,11.189L14.593,1.6L16.007,3.014L5.003,14.017L0,9.014Z" />
-                </svg>
-                <span className = 'text-white/60'>{waitlisted ? 'Added to waitlist' : 'Login link sent'}</span>
+                </svg> */}
+                <span className='text-white/60 text-xs uppercase mix-blend-difference' style={{ fontFamily: 'monospace' }}>{waitlisted ? 'Added' : 'Login link sent'}</span>
               </div>
               <button
                 onClick={() => window.open(waitlisted ? 'https://forms.gle/9wjkDzamRSeHVPRw5' : 'https://mail.google.com', '_blank')}
-                className="text-white hover:text-[#FF3001] transition-colors"
+                className="text-white hover:text-[#FF3001] transition-colors text-xs uppercase mix-blend-difference font-semibold"
               >
                 {waitlisted ? 'Jump Ahead ↵' : 'Open Gmail ↵'}
               </button>
             </div>
           )}
         </div>
+        </div>
+
+        {/* Dotted Line Above Image Section */}
+        <div className="custom-divider"></div>
+
+        {/* Image Section */}
+        <div className="flex gap-2 z-10 py-4 w-full">
+          {imageUrls.map((src, index) => (
+            <div key={index} className="flex-1">
+              <Image
+                className="opacity-70 hover:opacity-100 w-full h-auto"
+                src={src}
+                alt={`Image ${index + 1}`}
+                width={60}
+                height={60}
+                layout="responsive"
+              />
+            </div>
+          ))}
+        </div>
+
+        {/* Dotted Line Below Image Section */}
+        <div className="custom-divider"></div>
       </div>
 
+      {/* [Play] Div */}
+      <div 
+      className="absolute z-30 text-white px-1 py-1 rounded uppercase font-semibold text-xs"
+      style={{
+        left: cursorPosition.x + (cursorPosition.x > window.innerWidth - 100 ? -100 : 10) + 'px',
+        top: cursorPosition.y + (cursorPosition.y > window.innerHeight - 50 ? -30 : 10) + 'px',
+        pointerEvents: 'none'
+      }}
+    >
+      {cursorText}
+    </div>
+
       {/* Video Section */}
-      <div className="w-full max-w-5xl mb-16">
+      <div className="w-screen h-screen z-0 absolute inset-0">
         <video 
-          className="w-full rounded-lg shadow-lg"
+          className="w-screen h-screen rounded-lg shadow-lg absolute inset-0"
           autoPlay 
           loop 
           muted 
@@ -162,11 +319,6 @@ export default function Home() {
           Your browser does not support the video tag.
         </video>
       </div>
-
-      {/* Footer */}
-      <footer className="mt-auto pt-8 text-sm text-gray-400 relative bottom-4 right-4">
-        © Pixelparc LLC
-      </footer>
     </main>
   );
 }
