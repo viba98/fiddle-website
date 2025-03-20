e3d8b83cc9b69315e97f9e8dcda867bfa8bee15a
viba98 <32447352+viba98@users.noreply.github.com>
2025-03-20 14:29:35 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'YOUR_TRACKING_ID', {
+            gtag('config', 'G-1PHNXN8BZS', {
               page_path: window.location.pathname,
             });
           `}
